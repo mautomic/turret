89a24b6a1115f522a7febf4ff96a322636d7a315
mautomic <maurya373@gmail.com>
2020-07-06 01:57:15 -0500

    track average request time

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,38 @@
 use std::io::Read;
+use std::time::Instant;
 
 fn main() {
 
     let base_url: &str = "http://127.0.0.1:8500/employee/id/";
 
-    for x in 1..25 {
+    let mut total_time = 0;
+    let requests_to_fire:u128 = 50;
+
+    for x in 1..requests_to_fire {
         let url = &create_url(base_url, x);
         let mut response_body = String::new();
-        let result = hit_endpoint(url, &mut response_body);
+        let result = hit_endpoint(url, &mut response_body, &mut total_time);
         if result.is_ok() {
             println!("{:#?}", response_body);
         } else {
             println!("Error during GET request");
         }
     }
+
+    let avg_request_time = total_time/requests_to_fire;
+    println!("Average request time was {} ms", avg_request_time);
 }
 
-fn hit_endpoint(url: &str, response_body: &mut String) -> Result<(), Box<dyn std::error::Error>> {
+fn hit_endpoint(url: &str, response_body: &mut String, time: &mut u128) -> Result<(), Box<dyn std::error::Error>> {
 
+    let start = Instant::now();
     let mut resp = reqwest::blocking::get(url)?;
     resp.read_to_string(response_body)?;
+    *time = *time + start.elapsed().as_millis();
     Ok(())
 }
 
-fn create_url(base_url: &str, id: i32) -> String {
+fn create_url(base_url: &str, id: u128) -> String {
 
     let id: &str = &id.to_string();
     let url = format!("{}{}", base_url, id);
