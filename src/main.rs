995e0457be22565b40d8ed0bf4e4ba32b3ef186c
mautomic <maurya373@gmail.com>
2020-07-06 00:40:37 -0500

    move url creation to own function

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,21 @@ use std::io::Read;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
+    let base_url: &str = "http://127.0.0.1:8500/employee/id/";
+
     for x in 1..25 {
-        let id: &str = &x.to_string();
-        let url = format!("http://127.0.0.1:8500/employee/id/{}", id);
-        let mut resp = reqwest::blocking::get(&url)?;
+        let mut resp = reqwest::blocking::get(&create_url(base_url, x))?;
         let mut body = String::new();
         resp.read_to_string(&mut body)?;
         println!("{:#?}", body);
     }
 
     Ok(())
+}
+
+fn create_url(base_url: &str, id: i32) -> String {
+
+    let id: &str = &id.to_string();
+    let url = format!("{}{}", base_url, id);
+    return url;
 }
