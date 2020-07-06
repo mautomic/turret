37f9f27a67969e65878d03080f42581b13a5cac0
mautomic <maurya373@gmail.com>
2020-07-06 01:28:59 -0500

    move request logic to own function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 use std::io::Read;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
 
     let base_url: &str = "http://127.0.0.1:8500/employee/id/";
 
     for x in 1..25 {
-        let mut resp = reqwest::blocking::get(&create_url(base_url, x))?;
-        let mut body = String::new();
-        resp.read_to_string(&mut body)?;
-        println!("{:#?}", body);
+        let url = &create_url(base_url, x);
+        let mut response_body = String::new();
+        let result = hit_endpoint(url, &mut response_body);
+        if result.is_ok() {
+            println!("{:#?}", response_body);
+        } else {
+            println!("Error during GET request");
+        }
     }
+}
 
+fn hit_endpoint(url: &str, response_body: &mut String) -> Result<(), Box<dyn std::error::Error>> {
+
+    let mut resp = reqwest::blocking::get(url)?;
+    resp.read_to_string(response_body)?;
     Ok(())
 }
 
