6bb22b4ce723430acba4464752226466b43c4f37
mautomic <maurya373@gmail.com>
2020-07-05 23:04:55 -0500

    implement basic GET request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    for x in 1..25 {
+        let id: &str = &x.to_string();
+        let url = format!("http://127.0.0.1:8500/employee/id/{}", id);
+        let mut resp = reqwest::blocking::get(&url)?;
+        let mut body = String::new();
+        resp.read_to_string(&mut body)?;
+        println!("{:#?}", body);
+    }
+
+    Ok(())
 }
