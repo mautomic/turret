0eba7f8bfd5090b3db4e5d4c43b53eedbaf7a779
mautomic <maurya373@gmail.com>
2020-07-06 23:24:31 -0500

    add comments to program

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ use std::time::Instant;
 #[macro_use]
 extern crate clap;
 
+// Main function that kicks off turret
 fn main() {
 
+    // turret requires two command line args, -u for the url and -n for the number of requests
     let matches = clap_app!(myapp =>
         (version: "0.1.0")
         (author: "mautomic")
@@ -20,6 +22,10 @@ fn main() {
     deploy_turret(url, requests_to_fire);
 }
 
+// Function that fires a specified number of requests to the provided url
+// and keeps an accumulation of the total time the response takes to arrive
+// back. Once all requests are finished, the total time is divided by the num
+// of requests in order to get the average time.
 fn deploy_turret(base_url: &str, requests_to_fire: u128) {
 
     let mut total_time = 0;
@@ -35,6 +41,10 @@ fn deploy_turret(base_url: &str, requests_to_fire: u128) {
     println!("Average request time was {} ms", avg_request_time);
 }
 
+// Function that marks the time using Instant, makes a synchronous request
+// to the url, and writes the response to the provided response_body reference
+// and adds the time taken to the time reference. These variables are used in
+// the caller of this function.
 fn hit_endpoint(url: &str, response_body: &mut String, time: &mut u128) -> Result<(), Box<dyn std::error::Error>> {
     let start = Instant::now();
     let mut resp = reqwest::blocking::get(url)?;
